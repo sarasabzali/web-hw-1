85e459a2a36e5cc6b6b4281c73356f0be1353b0a
sarasabzali <66485122+sarasabzali@users.noreply.github.com>
2020-06-18 18:56:17 +0300

    ok

---
+++ script.js
@@ -0,0 +1,40 @@
+      alert ("مرحبا, شكرا لدخولك عالم المجرات ,استمتع")
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  var name = "momo";
+//  var age ="20";
+//  var ismarried = true;
+//  var country = "kuwait";
+
+// var id_1 = ["momo",20,true,"kuwait"]
+// console.log(id_1.indexOf(true))
+ 
+// var car = {
+
+// name:"BMW"
+// ,model:"2019"
+// ,color:"red"
+
+// };
+// document.getElementById("object").innerHTML="name: "+ car.name;
+
